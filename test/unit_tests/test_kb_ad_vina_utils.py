a388c8c4d977f188daf56ee2a74e4be5de065d1b
Dakota Blair <dblair@bnl.gov>
2022-07-13 16:36:19 -0400

    Refactored utility functions and added DFU client.
    Moved core obabel transformations into the utils module, along with the vina
    execution. Added the DataFileUtil client for use with ProteinStructureUtils.

--- test/unit_tests/test_kb_ad_vina_utils.py
+++ test/unit_tests/test_kb_ad_vina_utils.py
@@ -1,14 +1,11 @@
 import logging
+import math
 import os
 import subprocess
 
 import pytest
 
-from kb_ad_vina.utils import ADVinaApp
-
-
-def approximatelyEqual(float1, float2, epsilon):
-    return abs(float1 - float2) < epsilon
+from kb_ad_vina.utils import ligand_as_pdbqt, receptor_as_pdbqt, run_vina
 
 
 def clean():
@@ -27,8 +24,8 @@ MODULE_HOME = (
 )
 os.chdir(os.path.join(MODULE_HOME, "test/data"))
 affinity = {
-    "Structure2D_CID_49846579": -8.1,
-    "Structure2D_CID_139024764": -7.9,
+    "Structure2D_CID_49846579.sdf": -8.1,
+    "Structure2D_CID_139024764.sdf": -7.9,
 }
 clean()
 
@@ -40,32 +37,12 @@ def ligands():
 
 @pytest.fixture
 def receptor():
-    return "6wzu"
+    return "6wzu.pdb"
 
 
 def test_01_pdb_to_pdbqt(receptor):
-    obabel_cmd_receptor = (
-        f"obabel -i pdb {receptor}.pdb -o pdbqt -O {receptor}.obabel.pdbqt"
-    )
-    with subprocess.Popen(
-        obabel_cmd_receptor,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    ) as proc:
-        proc.communicate()
-
-    with subprocess.Popen(
-        f"""grep -e "^\\(ATOM\\|TER\\)" {receptor}.obabel.pdbqt \\
-            >> {receptor}.obabel.clean.pdbqt
-        """,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    ) as proc:
-        proc.communicate()
-
-    with open(f"{receptor}.obabel.clean.pdbqt") as f:
+    receptor_converted = receptor_as_pdbqt(receptor)
+    with open(receptor_converted) as f:
         lines = f.readlines()
     for line in lines:
         assert line.startswith("ATOM") or line.startswith("TER")
@@ -73,53 +50,26 @@ def test_01_pdb_to_pdbqt(receptor):
 
 def test_02_sdfs_to_pdbqt(ligands):
     for ligand in ligands:
-        ligand_obabel_cmd = (
-            f"obabel -i sdf {ligand}.sdf -o pdbqt -O {ligand}.obabel.pdbqt -r"
-        )
-        with subprocess.Popen(
-            ligand_obabel_cmd,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ) as proc:
-            proc.communicate()
-            ligand_proc = proc
+        ligand_pdbqt = ligand_as_pdbqt(ligand)
 
-        assert ligand_proc.returncode == 0
-
+        assert ligand_pdbqt == f"{ligand}.pdbqt"
 
 def test_03_vina(receptor, ligands):
     for ligand in ligands:
-        vina_cmd = f"""vina \\
-                --receptor {receptor}.obabel.clean.pdbqt \\
-                --ligand {ligand}.obabel.pdbqt \\
-                --center_x -7 \\
-                --center_y 78 \\
-                --center_z 38.6 \\
-                --size_x 34 \\
-                --size_y 30 \\
-                --size_z 22 \\
-                --out {receptor}-{ligand}.pdbqt \\
-                --log test.log \\
-                --seed 0\\
-                --exhaustiveness 2 \\
-                --num_modes 10 \\
-                --energy_range 10 \\
-            """
+        receptor_filename = f"{receptor}qt"
+        ligand_filename = f"{ligand}.pdbqt"
+        log_filename = f"{receptor}-{ligand}.log"
+        output, log = run_vina(receptor_filename, ligand_filename, ".", {})
         with subprocess.Popen(
-            vina_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        ) as proc:
-            stdout, stderr = proc.communicate()
-            logging.info(stdout.decode("utf-8", "ignore"))
-        with subprocess.Popen(
-            "head -n27 test.log | tail -n1 | awk '{ print $2 }'",
+            f"head -n27 {log} | tail -n1 | awk '{{ print $2 }}'",
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         ) as proc:
             stdout, stderr = proc.communicate()
-            assert approximatelyEqual(
+            assert len(stderr) == 0
+            assert math.isclose(
                 affinity[ligand],
                 float(stdout.decode("utf-8", "ignore")[:-1]),
-                EPSILON,
+                abs_tol=EPSILON,
             )
